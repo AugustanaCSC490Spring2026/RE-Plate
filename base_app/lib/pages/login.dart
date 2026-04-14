dec727c985c8a8ff4ebdf5a03a524b8e0fca6c30
Sonam <sonamgurung22@augustana.edu>
2026-04-14 11:05:15 -0500

    Polish login screen UI

--- base_app/lib/pages/login.dart
+++ base_app/lib/pages/login.dart
@@ -4,9 +4,6 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-// credits to @MahdiNazmi for source code
-// github link: 
-
 class Login extends StatelessWidget {
   Login({super.key});
 
@@ -22,21 +19,27 @@ class Login extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
-        toolbarHeight: 100,
+        toolbarHeight: 90,
+        leadingWidth: 72,
         leading: GestureDetector(
           onTap: () {
             Navigator.pop(context);
           },
           child: Container(
-            margin: const EdgeInsets.only(left: 10),
-            decoration: const BoxDecoration(
-              color: Color(0xffF7F7F9),
-              shape: BoxShape.circle
+            margin: const EdgeInsets.only(left: 16, top: 12, bottom: 12),
+            decoration: BoxDecoration(
+              color: const Color(0xffF7F7F9),
+              shape: BoxShape.circle,
+              border: Border.all(
+                color: const Color(0xffECECEC),
+                width: 1,
+              ),
             ),
             child: const Center(
               child: Icon(
                 Icons.arrow_back_ios_new_rounded,
-                color: Colors.black,
+                color: Colors.black87,
+                size: 18,
               ),
             ),
           ),
@@ -44,171 +47,256 @@ class Login extends StatelessWidget {
       ),
       body: SafeArea(
         child: SingleChildScrollView(
-         padding: const EdgeInsets.symmetric(horizontal: 16,vertical: 16),
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              const SizedBox(height: 12),
               Center(
-                child: Text(
-                  'Welcome back!',
-                  style: GoogleFonts.raleway(
-                    textStyle: const TextStyle(
-                      color: Colors.green,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 32
-                    )
-                  ),
+                child: Column(
+                  children: [
+                    Text(
+                      'Welcome back!',
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.raleway(
+                        textStyle: const TextStyle(
+                          color: Color(0xff4CAF50),
+                          fontWeight: FontWeight.w700,
+                          fontSize: 34,
+                          letterSpacing: 0.2,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      'Sign in to continue to your account',
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.raleway(
+                        textStyle: const TextStyle(
+                          color: Color(0xff8A8A8A),
+                          fontWeight: FontWeight.w500,
+                          fontSize: 14,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              const SizedBox(height: 80,),
-               _emailAddress(),
-               const SizedBox(height: 20,),
-               _password(context),
-               const SizedBox(height: 50,),
-               _signin(context),
+              const SizedBox(height: 56),
+              _emailAddress(),
+              const SizedBox(height: 24),
+              _password(context),
+              const SizedBox(height: 36),
+              _signin(context),
+              const SizedBox(height: 16),
             ],
           ),
         ),
       ),
     );
   }
-  
+
   Widget _emailAddress() {
     return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text(
           'Username or Email',
           style: GoogleFonts.raleway(
             textStyle: const TextStyle(
-              color: Colors.black,
-              fontWeight: FontWeight.normal,
-              fontSize: 16
-            )
+              color: Colors.black87,
+              fontWeight: FontWeight.w600,
+              fontSize: 16,
+            ),
           ),
         ),
-        const SizedBox(height: 16,),
+        const SizedBox(height: 12),
         TextField(
           controller: _emailController,
+          style: GoogleFonts.raleway(
+            textStyle: const TextStyle(
+              color: Colors.black87,
+              fontSize: 15,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
           decoration: InputDecoration(
+            prefixIcon: const Icon(
+              Icons.person_outline_rounded,
+              color: Color(0xff8A8A8A),
+            ),
+            contentPadding: const EdgeInsets.symmetric(
+              vertical: 20,
+              horizontal: 16,
+            ),
             filled: true,
             hintText: 'Enter your username or email',
-            hintStyle: const TextStyle(
-              color: Color(0xff6A6A6A),
-              fontWeight: FontWeight.normal,
-              fontSize: 14
+            hintStyle: GoogleFonts.raleway(
+              textStyle: const TextStyle(
+                color: Color(0xff9A9A9A),
+                fontWeight: FontWeight.w500,
+                fontSize: 14,
+              ),
             ),
-            fillColor: const Color(0xffF7F7F9) ,
-            border: OutlineInputBorder(
-              borderSide: BorderSide.none,
-              borderRadius: BorderRadius.circular(14)
-            )
+            fillColor: const Color(0xffF7F7F9),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(18),
+              borderSide: const BorderSide(
+                color: Color(0xffEEEEEE),
+                width: 1,
+              ),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(18),
+              borderSide: const BorderSide(
+                color: Color(0xff4CAF50),
+                width: 1.2,
+              ),
+            ),
           ),
-        )
+        ),
       ],
     );
   }
 
-Widget _password(BuildContext context) {
-  return Column(
-    mainAxisAlignment: MainAxisAlignment.start,
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-      Text(
-        'Password',
-        style: GoogleFonts.raleway(
-          textStyle: const TextStyle(
-            color: Colors.black,
-            fontWeight: FontWeight.normal,
-            fontSize: 16
-          )
+  Widget _password(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Password',
+          style: GoogleFonts.raleway(
+            textStyle: const TextStyle(
+              color: Colors.black87,
+              fontWeight: FontWeight.w600,
+              fontSize: 16,
+            ),
+          ),
         ),
-      ),
-      const SizedBox(height: 16,),
-      TextField(
-        obscureText: true,
-        controller: _passwordController,
-          onSubmitted: (_) async {       
+        const SizedBox(height: 12),
+        TextField(
+          obscureText: true,
+          controller: _passwordController,
+          style: GoogleFonts.raleway(
+            textStyle: const TextStyle(
+              color: Colors.black87,
+              fontSize: 15,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          onSubmitted: (_) async {
             await AuthService().signin(
               emailOrUsername: _emailController.text,
               password: _passwordController.text,
               context: context,
             );
           },
-        decoration: InputDecoration(
-          filled: true,
-          hintText: 'Enter your password',
-          hintStyle: const TextStyle(
-            color: Color(0xff6A6A6A),
-            fontWeight: FontWeight.normal,
-            fontSize: 14,
+          decoration: InputDecoration(
+            prefixIcon: const Icon(
+              Icons.lock_outline_rounded,
+              color: Color(0xff8A8A8A),
+            ),
+            contentPadding: const EdgeInsets.symmetric(
+              vertical: 20,
+              horizontal: 16,
+            ),
+            filled: true,
+            hintText: 'Enter your password',
+            hintStyle: GoogleFonts.raleway(
+              textStyle: const TextStyle(
+                color: Color(0xff9A9A9A),
+                fontWeight: FontWeight.w500,
+                fontSize: 14,
+              ),
+            ),
+            fillColor: const Color(0xffF7F7F9),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(18),
+              borderSide: const BorderSide(
+                color: Color(0xffEEEEEE),
+                width: 1,
+              ),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(18),
+              borderSide: const BorderSide(
+                color: Color(0xff4CAF50),
+                width: 1.2,
+              ),
+            ),
           ),
-          fillColor: const Color(0xffF7F7F9),
-          border: OutlineInputBorder(
-            borderSide: BorderSide.none,
-            borderRadius: BorderRadius.circular(14)
-          )
         ),
-      )
-    ],
-  );
-}
+      ],
+    );
+  }
 
   Widget _signin(BuildContext context) {
     return ElevatedButton(
       style: ElevatedButton.styleFrom(
         backgroundColor: const Color.fromARGB(255, 13, 253, 173),
+        foregroundColor: Colors.white,
         shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(14),
+          borderRadius: BorderRadius.circular(18),
         ),
-        minimumSize: const Size(double.infinity, 60),
+        minimumSize: const Size(double.infinity, 62),
         elevation: 0,
       ),
       onPressed: () async {
         await AuthService().signin(
           emailOrUsername: _emailController.text,
           password: _passwordController.text,
-          context: context
+          context: context,
         );
       },
-      child: const Text("Sign In"),
+      child: Text(
+        "Sign In",
+        style: GoogleFonts.raleway(
+          textStyle: const TextStyle(
+            color: Colors.white,
+            fontSize: 18,
+            fontWeight: FontWeight.w700,
+            letterSpacing: 0.3,
+          ),
+        ),
+      ),
     );
   }
 
   Widget _signup(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(bottom: 16),
+      padding: const EdgeInsets.only(bottom: 28, left: 16, right: 16, top: 8),
       child: RichText(
         textAlign: TextAlign.center,
         text: TextSpan(
+          style: GoogleFonts.raleway(),
           children: [
             const TextSpan(
-                text: "New User? ",
-                style: TextStyle(
-                  color: Color(0xff6A6A6A),
-                  fontWeight: FontWeight.normal,
-                  fontSize: 16
-                ),
+              text: "New User? ",
+              style: TextStyle(
+                color: Color(0xff8A8A8A),
+                fontWeight: FontWeight.w500,
+                fontSize: 16,
               ),
-              TextSpan(
-                text: "Create Account",
-                style: const TextStyle(
-                    color: Color(0xff1A1D1E),
-                    fontWeight: FontWeight.normal,
-                    fontSize: 16
-                  ),
-                  recognizer: TapGestureRecognizer()..onTap = () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => Signup()
-                      ),
-                    );
-                  }
+            ),
+            TextSpan(
+              text: "Create Account",
+              style: const TextStyle(
+                color: Color(0xff1A1D1E),
+                fontWeight: FontWeight.w700,
+                fontSize: 16,
               ),
-          ]
-        )
+              recognizer: TapGestureRecognizer()
+                ..onTap = () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => Signup(),
+                    ),
+                  );
+                },
+            ),
+          ],
+        ),
       ),
     );
   }
